190410cc2a769c9b148ee858592619ed1e9257ea
Eduardo Miquilena <edumqr1@gmail.com>
2020-10-23 15:47:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 
 module.exports = function isStrHex(str){
     for (var i = 0; i < str.length; i++) {
-let char = str.charAt(i);
-var code = typeof char === "string" ? char.charCodeAt(0) : char
-//  console.log(code);
-    if(
-        
-        (code >= 48  && code <= 57)  ||
-      (code >= 65  && code <= 70)  ||
-      (code >= 97  && code <= 102) 
-    ) {
-    } else{
+        let c = str.charAt(i);
+let char = parseInt(str.charAt(i), 16);
+
+
+  
+
+
+    if(char.toString(16) !== c.toLowerCase()) {
+
 return false;
     }
 
